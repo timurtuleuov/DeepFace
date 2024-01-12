7db9fdec9eee8c99594253744391901d106de464
Timur Tuleuov <timmmur123@gmail.com>
2024-01-12 14:34:43 +0600

    start: init project, made jwt auth system

---
+++ src/main/java/com/timur/deepface/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.timur.deepface.controller;
+
+import com.timur.deepface.dto.JwtAuthenticationResponse;
+import com.timur.deepface.dto.SignInRequest;
+import com.timur.deepface.dto.SignUpRequest;
+import com.timur.deepface.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Регистрация пользователя")
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @Operation(summary = "Авторизация пользователя")
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
---
+++ src/test/java/com/timur/deepface/DeepfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timur.deepface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
